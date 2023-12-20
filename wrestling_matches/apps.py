3893e6f941b181f1d4b01aa81f4ecd48f85a2ab9
pablo-cs <pcrisostomosuarez5@gmail.com>
2023-12-20 14:07:26 -0500

    Updated models and reset apps.py for wrestling_matches DB scraping

--- wrestling_matches/apps.py
+++ wrestling_matches/apps.py
@@ -1,6 +1,26 @@
 from django.apps import AppConfig
+from bs4 import BeautifulSoup
+import requests
+from models import (
+    Wrestler,
+    Match,
+    MatchParticipant,
+    Venue,
+    Promotion,
+    TagTeam,
+    RingName,
+)
+
+base_url = "http://www.profightdb.com/"
+response = requests.get(url)
+soup = BeautifulSoup(response.content, "html.parser")
 
 
 class WrestlingMatchesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'wrestling_matches'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "wrestling_matches"
+
+
+# Import necessary libraries
+import requests
+from bs4 import BeautifulSoup
--- wrestling_matches/models.py
+++ wrestling_matches/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from datetime import timedelta
+from datetime import date
 
 # Create your models here.
+
+
+class Promotion(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Venue(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
+    promotion = models.ForeignKey(Promotion, on_delete=models.CASCADE)
+    date = models.DateField(default=date.today)
+
+
+class Match(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, default=1)
+    length = models.DurationField(default=timedelta(days=0))
+    stipulation = models.CharField(max_length=50, null=True, blank=True)
+    title = models.CharField(max_length=100)
+
+
+class TagTeam(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Wrestler(models.Model):
+    site_id = models.CharField(max_length=100, primary_key=True)
+    name = models.CharField(max_length=100)
+
+
+class RingName(models.Model):
+    name = models.CharField(max_length=100)
+    wrestler = models.ForeignKey(Wrestler, on_delete=models.CASCADE)
+
+
+class MatchParticipant(models.Model):
+    ring_name = models.ForeignKey(RingName, on_delete=models.CASCADE)
+    match = models.ForeignKey(Match, on_delete=models.CASCADE)
+    is_tag_team = models.BooleanField(default=False)
+    tag_team = models.ForeignKey(TagTeam, on_delete=models.CASCADE, null=True)
+    winner = models.BooleanField(default=False)
