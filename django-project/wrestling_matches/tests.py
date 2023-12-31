cfa38b57ca77668a02703ca7c29de28fba2acadf
pablo-cs <pcrisostomosuarez5@gmail.com>
2023-12-31 02:21:15 -0500

    Began establishing testing setUp for views.py

--- django-project/wrestling_matches/tests.py
+++ django-project/wrestling_matches/tests.py
@@ -1,3 +1,175 @@
 from django.test import TestCase
 
 # Create your tests here.
+import unittest
+from datetime import date, timedelta
+from .models import (
+    Promotion,
+    Venue,
+    Event,
+    Match,
+    Title,
+    TagTeam,
+    Wrestler,
+    RingName,
+    MatchParticipant,
+)
+
+
+class TestWrestlingViews(unittest.TestCase):
+    def setUp(self):
+        # Create Promotions
+        promotion1 = Promotion.objects.create(name="Promotion 1")
+        promotion2 = Promotion.objects.create(name="Promotion 2")
+
+        # Create Venues
+        venue1 = Venue.objects.create(name="Venue 1", location="Location 1")
+        venue2 = Venue.objects.create(name="Venue 2", location="Location 2")
+
+        # Create Events
+        event1 = Event.objects.create(
+            site_id="event_1",
+            name="Event 1",
+            venue=venue1,
+            promotion=promotion1,
+            date=date.today(),
+        )
+        event2 = Event.objects.create(
+            site_id="event_2",
+            name="Event 2",
+            venue=venue2,
+            promotion=promotion2,
+            date=date.today(),
+        )
+
+        # Create Matches
+        match1 = Match.objects.create(
+            name="Match 1", event=event1, duration=timedelta(hours=1)
+        )
+        match2 = Match.objects.create(
+            name="Match 2", event=event2, duration=timedelta(hours=2)
+        )
+
+        # Create Titles
+        title1 = Title.objects.create(name="Title 1")
+        title2 = Title.objects.create(name="Title 2")
+
+        # Assign Matches to Titles
+        title1.matches.add(match1)
+        title2.matches.add(match2)
+
+        # Create Wrestlers
+        wrestler1 = Wrestler.objects.create(site_id="wrestler_1", name="Wrestler 1")
+        wrestler2 = Wrestler.objects.create(site_id="wrestler_2", name="Wrestler 2")
+
+        # Create Ring Names
+        ring_name1 = RingName.objects.create(name="Ring Name 1", wrestler=wrestler1)
+        ring_name2 = RingName.objects.create(name="Ring Name 2", wrestler=wrestler2)
+
+        # Create Tag Teams
+        tag_team1 = TagTeam.objects.create(name="Tag Team 1")
+        tag_team2 = TagTeam.objects.create(name="Tag Team 2")
+
+        # Add Wrestlers to Tag Teams
+        tag_team1.wrestlers.add(wrestler1, wrestler2)
+        tag_team2.wrestlers.add(wrestler2)
+
+        # Create Match Participants
+        participant1 = MatchParticipant.objects.create(
+            ring_name=ring_name1,
+            match=match1,
+            is_tag_team=False,
+            winner=True,
+            champion=True,
+        )
+        participant2 = MatchParticipant.objects.create(
+            ring_name=ring_name2, match=match2, is_tag_team=True, tag_team=tag_team2
+        )
+
+    def test_wrestler_view(self):
+        # Test the wrestler_view function
+        pass
+
+    def test_search_wrestler(self):
+        # Test the search_wrestler function
+        pass
+
+    def test_match_view(self):
+        # Test the match_view function
+        pass
+
+    def test_search_match(self):
+        # Test the search_match function
+        pass
+
+    def test_promotion_view(self):
+        # Test the promotion_view function
+        pass
+
+    def test_search_promotion(self):
+        # Test the search_promotion function
+        pass
+
+    def test_venue_view(self):
+        # Test the venue_view function
+        pass
+
+    def test_search_venue(self):
+        # Test the search_venue function
+        pass
+
+    def test_tag_team_view(self):
+        # Test the tag_team_view function
+        pass
+
+    def test_search_tag_team(self):
+        # Test the search_tag_team function
+        pass
+
+    def test_title_view(self):
+        # Test the title_view function
+        pass
+
+    def test_search_title(self):
+        # Test the search_title function
+        pass
+
+    def test_event_view(self):
+        # Test the event_view function
+        pass
+
+    def test_search_event(self):
+        # Test the search_event function
+        pass
+
+    def test_wrestler_matches(self):
+        # Test the wrestler_matches function
+        pass
+
+    def test_event_matches(self):
+        # Test the event_matches function
+        pass
+
+    def test_tag_team_matches(self):
+        # Test the tag_team_matches function
+        pass
+
+    def test_title_matches(self):
+        # Test the title_matches function
+        pass
+
+    def test_venue_events(self):
+        # Test the venue_events function
+        pass
+
+    def test_promotion_events(self):
+        # Test the promotion_events function
+        pass
+
+    def tearDown(self):
+        # Clean up any resources, perform cleanup steps after each test if needed
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
