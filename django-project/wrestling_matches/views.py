8ab69cf8f19decf75aef652870c2e75ade9a65e6
pablo-cs <pcrisostomosuarez5@gmail.com>
2024-01-02 19:15:09 -0500

    Add get_cached_object function

--- django-project/wrestling_matches/views.py
+++ django-project/wrestling_matches/views.py
@@ -334,28 +334,27 @@ def search_by_query(model, query, page_number=1, items_per_page=10, **kwargs):
         return []
 
 
-def wrestler_view(request, wrestler_id):
+def wrestler_view(request):
     """
     Retrieve details of a specific Wrestler.
 
     Args:
         request: The request object.
-        wrestler_id: ID of the Wrestler to retrieve.
 
     Returns:
         Details of the Wrestler in JSON format.
     """
-
-    try:
-        cache_key = f"wrestler_{wrestler_id}"
-        wrestler = cache.get(cache_key)
-        if not wrestler:
-            wrestler = Wrestler.objects.get(site_id=wrestler_id)
-            cache.set(cache_key, wrestler, timeout=3600)  # Cache for 1 hour
-        serializer = WrestlerSerializer(wrestler)
-        return JsonResponse(serializer.data, safe=False)
-    except Wrestler.DoesNotExist:
-        return JsonResponse({"error": "Wrestler not found"}, status=404)
+    wrestler_id = request.GET.get("wrestler_id")
+    if wrestler_id:
+        try:
+            cache_key = f"wrestler_{wrestler_id}"
+            wrestler = get_cached_object(cache_key, Wrestler, wrestler_id)
+            serializer = WrestlerSerializer(wrestler)
+            return JsonResponse(serializer.data, safe=False)
+        except Wrestler.DoesNotExist:
+            return JsonResponse({"error": "Wrestler not found"}, status=404)
+    else:
+        return JsonResponse({"error": "Invalid Arguments"}, status=404)
 
 
 def search_wrestler(request):
@@ -374,28 +373,28 @@ def search_wrestler(request):
     )
 
 
-def match_view(request, match_id):
+def match_view(request):
     """
     Retrieve details of a specific Match.
 
     Args:
         request: The request object.
-        match_id: ID of the Match to retrieve.
 
     Returns:
         Details of the Match in JSON format.
     """
 
-    try:
-        cache_key = f"match{match_id}"
-        match = cache.get(cache_key)
-        if not match:
-            match = Match.objects.get(id=match_id)
-            cache.set(cache_key, match, timeout=3600)
-        serializer = MatchSerializer(match)
-        return JsonResponse(serializer.data, safe=False)
-    except Match.DoesNotExist:
-        return JsonResponse({"error": "Match not found"}, status=404)
+    match_id = request.GET.get("match_id")
+    if match_id:
+        try:
+            cache_key = f"match{match_id}"
+            match = get_cached_object(cache_key, Match, match_id)
+            serializer = MatchSerializer(match)
+            return JsonResponse(serializer.data, safe=False)
+        except Match.DoesNotExist:
+            return JsonResponse({"error": "Match not found"}, status=404)
+    else:
+        return JsonResponse({"error": "Invalid Arguments"}, status=404)
 
 
 def search_match(request):
@@ -415,28 +414,27 @@ def search_match(request):
     )
 
 
-def promotion_view(request, promotion_id):
+def promotion_view(request):
     """
     Retrieve details of a specific Promotion.
 
     Args:
         request: The request object.
-        promotion_id: ID of the Promotion to retrieve.
 
     Returns:
         Details of the Promotion in JSON format.
     """
-
-    try:
-        cache_key = f"promotion{promotion_id}"
-        promotion = cache.get(cache_key)
-        if not promotion:
-            promotion = Promotion.objects.get(id=promotion_id)
-            cache.set(cache_key, promotion, timeout=3600)
-        serializer = PromotionSerializer(promotion)
-        return JsonResponse(serializer.data, safe=False)
-    except Promotion.DoesNotExist:
-        return JsonResponse({"error": "Promotion not found"}, status=404)
+    promotion_id = request.GET.get("promotion_id")
+    if promotion_id:
+        try:
+            cache_key = f"promotion{promotion_id}"
+            promotion = get_cached_object(cache_key, Promotion, promotion_id)
+            serializer = PromotionSerializer(promotion)
+            return JsonResponse(serializer.data, safe=False)
+        except Promotion.DoesNotExist:
+            return JsonResponse({"error": "Promotion not found"}, status=404)
+    else:
+        return JsonResponse({"error": "Invalid Arguments"}, status=404)
 
 
 def search_promotion(request):
@@ -455,27 +453,27 @@ def search_promotion(request):
     )
 
 
-def venue_view(request, venue_id):
+def venue_view(request):
     """
     Retrieve details of a specific Venue.
 
     Args:
         request: The request object.
-        venue_id: ID of the Venue to retrieve.
 
     Returns:
         Details of the Venue in JSON format.
     """
-    try:
-        cache_key = f"venue{venue_id}"
-        venue = cache.get(cache_key)
-        if not venue:
-            venue = Venue.objects.get(id=venue_id)
-            cache.set(cache_key, venue, timeout=3600)
-        serializer = VenueSerializer(venue)
-        return JsonResponse(serializer.data, safe=False)
-    except Venue.DoesNotExist:
-        return JsonResponse({"error": "Venue not found"}, status=404)
+    venue_id = request.GET.get("venue_id")
+    if venue_id:
+        try:
+            cache_key = f"venue{venue_id}"
+            venue = get_cached_object(cache_key, Venue, venue_id)
+            serializer = VenueSerializer(venue)
+            return JsonResponse(serializer.data, safe=False)
+        except Venue.DoesNotExist:
+            return JsonResponse({"error": "Venue not found"}, status=404)
+    else:
+        return JsonResponse({"error": "Invalid Arguments"}, status=404)
 
 
 def search_venue(request):
@@ -495,27 +493,27 @@ def search_venue(request):
     )
 
 
-def tag_team_view(request, tag_team_id):
+def tag_team_view(request):
     """
     Retrieve details of a specific TagTeam.
 
     Args:
         request: The request object.
-        tag_team_id: ID of the TagTeam to retrieve.
 
     Returns:
         Details of the TagTeam in JSON format.
     """
-    try:
-        cache_key = f"tag_team{tag_team_id}"
-        tag_team = cache.get(cache_key)
-        if not tag_team:
-            tag_team = TagTeam.objects.get(id=tag_team_id)
-            cache.set(cache_key, tag_team, timeout=3600)
-        serializer = TagTeamSerializer(tag_team)
-        return JsonResponse(serializer.data, safe=False)
-    except TagTeam.DoesNotExist:
-        return JsonResponse({"error": "Tag Team not found"}, status=404)
+    tag_team_id = request.GET.get("tag_team_id")
+    if tag_team_id:
+        try:
+            cache_key = f"tag_team{tag_team_id}"
+            tag_team = get_cached_object(cache_key, TagTeam, tag_team_id)
+            serializer = TagTeamSerializer(tag_team)
+            return JsonResponse(serializer.data, safe=False)
+        except TagTeam.DoesNotExist:
+            return JsonResponse({"error": "Tag Team not found"}, status=404)
+    else:
+        return JsonResponse({"error": "Invalid Arguments"}, status=404)
 
 
 def search_tag_team(request):
@@ -535,27 +533,27 @@ def search_tag_team(request):
     )
 
 
-def title_view(request, title_id):
+def title_view(request):
     """
     Retrieve details of a specific Title.
 
     Args:
         request: The request object.
-        title_id: ID of the Title to retrieve.
 
     Returns:
         Details of the Title in JSON format.
     """
-    try:
-        cache_key = f"title{title_id}"
-        title = cache.get(cache_key)
-        if not title:
-            title = Title.objects.get(id=title_id)
-            cache.set(cache_key, title, timeout=3600)
-        serializer = TitleSerializer(title)
-        return JsonResponse(serializer.data, safe=False)
-    except Title.DoesNotExist:
-        return JsonResponse({"error": "Title not found"}, status=404)
+    title_id = request.GET.get("title_id")
+    if title_id:
+        try:
+            cache_key = f"title{title_id}"
+            title = get_cached_object(cache_key, Title, title_id)
+            serializer = TitleSerializer(title)
+            return JsonResponse(serializer.data, safe=False)
+        except Title.DoesNotExist:
+            return JsonResponse({"error": "Title not found"}, status=404)
+    else:
+        return JsonResponse({"error": "Invalid Arguments"}, status=404)
 
 
 def search_title(request):
@@ -576,27 +574,27 @@ def search_title(request):
     )
 
 
-def event_view(request, event_id):
+def event_view(request):
     """
     Retrieve details of a specific Event.
 
     Args:
         request: The request object.
-        event_id: ID of the Event to retrieve.
 
     Returns:
         Details of the Event in JSON format.
     """
-    try:
-        cache_key = f"event{event_id}"
-        event = cache.get(cache_key)
-        if not event:
-            event = Event.objects.get(site_id=event_id)
-            cache.set(cache_key, event, timeout=3600)
-        serializer = EventSerializer(event)
-        return JsonResponse(serializer.data, safe=False)
-    except Event.DoesNotExist:
-        return JsonResponse({"error": "Event not found"}, status=404)
+    event_id = request.GET.get("event_id")
+    if event_id:
+        try:
+            cache_key = f"event{event_id}"
+            event = get_cached_object(cache_key, Event, event_id)
+            serializer = EventSerializer(event)
+            return JsonResponse(serializer.data, safe=False)
+        except Event.DoesNotExist:
+            return JsonResponse({"error": "Event not found"}, status=404)
+    else:
+        return JsonResponse({"error": "Invalid Arguments"}, status=404)
 
 
 def search_event(request):
@@ -616,88 +614,106 @@ def search_event(request):
     )
 
 
-def wrestler_matches(request, wrestler_id):
+def wrestler_matches(request):
     """
     Retrieve matches related to a specific Wrestler.
 
     Args:
         request: The request object.
-        wrestler_id: ID of the Wrestler.
 
     Returns:
         JSON response containing matches associated with the Wrestler.
     """
-    return get_info(request, wrestler_id, Wrestler, Match)
+    wrestler_id = request.GET.get("wrestler_id")
+    if wrestler_id:
+        return get_info(request, wrestler_id, Wrestler, Match)
+    else:
+        return JsonResponse({"error": "Invalid Arguments"}, status=404)
 
 
-def event_matches(request, event_id):
+def event_matches(request):
     """
     Retrieve matches related to a specific Event.
 
     Args:
         request: The request object.
-        event_id: ID of the Event.
 
     Returns:
         JSON response containing matches associated with the Event.
     """
-    return get_info(request, event_id, Event, Match)
+    event_id = request.GET.get("event_id")
+    if event_id:
+        return get_info(request, event_id, Event, Match)
+    else:
+        return JsonResponse({"error": "Invalid Arguments"}, status=404)
 
 
-def tag_team_matches(request, tag_team_id):
+def tag_team_matches(request):
     """
     Retrieve matches related to a specific Tag Team.
 
     Args:
         request: The request object.
-        tag_team_id: ID of the Tag Team.
 
     Returns:
         JSON response containing matches associated with the Tag Team.
     """
-    return get_info(request, tag_team_id, TagTeam, Match)
+    tag_team_id = request.GET.get("tag_team_id")
+    if tag_team_id:
+        return get_info(request, tag_team_id, TagTeam, Match)
+    else:
+        return JsonResponse({"error": "Invalid Arguments"}, status=404)
 
 
-def title_matches(request, title_id):
+def title_matches(request):
     """
     Retrieve matches related to a specific Title.
 
     Args:
         request: The request object.
-        title_id: ID of the Title.
 
     Returns:
         JSON response containing matches associated with the Title.
     """
-    return get_info(request, title_id, Title, Match)
+    title_id = request.GET.get("title_id")
+    if title_id:
+        return get_info(request, title_id, Title, Match)
+    else:
+        return JsonResponse({"error": "Invalid Arguments"}, status=404)
 
 
-def venue_events(request, venue_id):
+def venue_events(request):
     """
     Retrieve events related to a specific Venue.
 
     Args:
         request: The request object.
-        venue_id: ID of the Venue.
 
     Returns:
         JSON response containing events associated with the Venue.
     """
-    return get_info(request, venue_id, Venue, Event)
+    venue_id = request.GET.get("venue_id")
+    if venue_id:
+        return get_info(request, venue_id, Venue, Event)
+    else:
+        return JsonResponse({"error": "Invalid Arguments"}, status=404)
 
 
-def promotion_events(request, promotion_id):
+def promotion_events(request):
     """
     Retrieve events related to a specific Promotion.
 
     Args:
         request: The request object.
-        promotion_id: ID of the Promotion.
 
     Returns:
         JSON response containing events associated with the Promotion.
     """
-    return get_info(request, promotion_id, Promotion, Event)
+    promotion_id = request.GET.get("promotion_id")
+    if promotion_id:
+        return get_info(request, promotion_id, Promotion, Event)
+    else:
+        return JsonResponse({"error": "Invalid Arguments"}, status=404)
 
 
 def get_info(request, id, model, retrieve):
@@ -722,7 +738,10 @@ def get_info(request, id, model, retrieve):
         Promotion: ["promotion", PromotionSerializer],
     }
 
-    retr_info = {Match: MatchSerializer, Event: EventSerializer}
+    retr_info = {
+        Match: ["matches", MatchSerializer],
+        Event: ["events", EventSerializer],
+    }
 
     model_str = model_info[model][0]
     cache_key = f"{model_str}_{id}"
@@ -742,14 +761,19 @@ def get_info(request, id, model, retrieve):
     model_serializer = model_serializer_type(model_obj)
 
     paginate_info = None
+    paginate_str = retr_info[retrieve][0]
+    paginate_serializer_type = retr_info[retrieve][1]
     if retrieve == Match:
         paginate_info = model_serializer.get_matches(model_obj)
+        paginate_str
     else:
         paginate_info = model_serializer.get_events(model_obj)
 
-    paginated_info = get_paginated_response(paginate_info, retr_info[retrieve], request)
+    paginated_info = get_paginated_response(
+        paginate_info, paginate_serializer_type, request
+    )
 
-    return JsonResponse({"matches": paginated_info}, safe=False)
+    return JsonResponse({f"{paginate_str}": paginated_info}, safe=False)
 
 
 def get_paginated_response(queryset, serializer_class, request):
@@ -766,6 +790,34 @@ def get_paginated_response(queryset, serializer_class, request):
     """
     paginator = Paginator(queryset, per_page=10)  # Adjust per_page as needed
     page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
-    serialized_data = serializer_class(page_obj, many=True).data
+    try:
+        page_obj = paginator.page(page_number)
+        serialized_data = serializer_class(page_obj, many=True).data
+    except EmptyPage:
+        serialized_data = []
     return serialized_data
+
+
+def get_cached_object(cache_key, model, id):
+    """
+    Retrieve an object from cache or database and cache it.
+
+    Args:
+        cache_key: The key to use for caching.
+        model: The Django model for the object.
+        id: ID of the object.
+
+    Returns:
+        The cached object or None if not found.
+    """
+    cached_obj = cache.get(cache_key)
+    if not cached_obj:
+        try:
+            if model == Wrestler or model == Event:
+                cached_obj = model.objects.get(site_id=id)
+            else:
+                cached_obj = model.objects.get(id=id)
+            cache.set(cache_key, cached_obj, timeout=3600)  # Cache for 1 hour
+        except model.DoesNotExist:
+            cached_obj = None
+    return cached_obj
